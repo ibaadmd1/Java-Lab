d8b125dac6eca15f30f0eb543da0edc80d24b910
ibaadmd1 <lnu.ib@northeastern.edu>
2024-02-02 17:42:23 -0500

    Lab-

---
+++ javaSwings/src/MainPackage/User.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MainPackage;
+
+/**
+ *
+ * @author Ibaad Mohammed
+ */
+public class User {
+    String firstName;
+    String lastName;
+    String age;
+    String phoneNumber;
+    String emailId;
+    String getLastName;
+    
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    
+    
+}
