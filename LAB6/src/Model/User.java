ce54ac16375d1a43cb9c8db3826947a4bf801e2b
ibaadmd1 <lnu.ib@northeastern.edu>
2024-03-23 23:16:06 -0400

    Lab 6 Assignment

---
+++ LAB6/src/Model/User.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+import javax.swing.ButtonModel;
+
+/**
+ *
+ * @author Ibaad Mohammed
+ */
+public class User {
+   private String name;
+     private String age;
+     private int id;
+    
+
+   public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+   
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+     
+
+    
+ 
+    
+    
+}
